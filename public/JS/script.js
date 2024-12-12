526bec112629b7210784fa174b5eecf6db19e426
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2024-12-12 20:48:57 +0700

    Create script.js

---
+++ public/JS/script.js
@@ -0,0 +1,4 @@
+// script.js
+document.getElementById('changeColorBtn').addEventListener('click', function() {
+    document.body.style.backgroundColor = document.body.style.backgroundColor === 'lightgray' ? '#f4f4f4' : 'lightgray';
+});
