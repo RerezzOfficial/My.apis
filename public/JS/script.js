c3ea0854cd51c164ad4e783bc783856936f7cba1
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2024-12-14 21:44:49 +0700

    Update script.js

--- public/JS/script.js
+++ public/JS/script.js
@@ -2,3 +2,17 @@
 document.getElementById('changeColorBtn').addEventListener('click', function() {
     document.body.style.backgroundColor = document.body.style.backgroundColor === 'lightgray' ? '#f4f4f4' : 'lightgray';
 });
+
+        // Get battery status
+        navigator.getBattery().then(function(battery) {
+            const batteryStatus = document.getElementById('battery-status');
+            batteryStatus.textContent = `Baterai: ${Math.round(battery.level * 100)}%`;
+        });
+
+        // Get IP address
+        fetch('https://api.ipify.org?format=json')
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('ip-address').textContent = `IP Address: ${data.ip}`;
+            });
+                        
