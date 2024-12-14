0f2399fe864aba3f1ddbea9c6313427df3f772e8
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2024-12-14 21:40:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 // Endpoint untuk servis dokumen HTML
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public','index.html'));
 });
 app.get('/dashboard', (req, res) => {
   res.sendFile(path.join(__dirname, 'feature.html'));
