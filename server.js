287b7b68a366991e61055a4e68218a5197af6b15
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2024-12-14 21:27:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,29 +13,54 @@ app.listen(PORT, () => {
     console.log(`Server berjalan di http://localhost:${PORT}`);
 });
 
+app.get('/api/orkut/createpayment', async (req, res) => {
+    const { apikey } = req.query;    
+    if (apikey !== 'linebaik') {
+    return res.status(403).json({ error: "Isi Parameter Apikey" });
+    }
+    const { amount } = req.query;
+    if (!amount) {
+    return res.json("Isi Parameter Amount.");
+    }
+    const { codeqr } = req.query;
+    if (!codeqr) {
+    return res.json("Isi Parameter CodeQr menggunakan qris code kalian.");
+    }
+    try {
+        const qrData = await createQRIS(amount, codeqr);
+        res.json({ status: true, creator: "Line", result: qrData });        
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
-async function nweButton(vreden) {
-    const ownerBot = `6285216955233@s.whatsapp.net`;
+
+app.get('/api/orkut/cekstatus', async (req, res) => {
+    const { apikey, merchant, keyorkut } = req.query;
     
-    var msg = generateWAMessageFromContent(ownerBot, proto.Message.fromObject({
-        'viewOnceMessage': {
-            'message': {
-                'liveLocationMessage': {
-                    'degreesLatitude': 'p', 
-                    'degreesLongitude': 'p',
-                    "caption": "CONECTION",
-                    'sequenceNumber': '0',
-                    'jpegThumbnail': ''
-                }
-            }
+    if (apikey !== 'linebaik') {
+    return res.status(403).json({ error: "Isi Parameter Apikey" });
+    }
+    
+        if (!merchant) {
+        return res.json({ error: "Isi Parameter Merchant." });
+    }
+    if (!keyorkut) {
+        return res.json({ error: "Isi Parameter Token menggunakan token kalian." });
+    }
+    try {
+        const apiUrl = `https://gateway.okeconnect.com/api/mutasi/qris/${merchant}/${keyorkut}`;
+        const response = await axios.get(apiUrl);
+        const result = response.data;
+                // Check if data exists and get the latest transaction
+        const latestTransaction = result.data && result.data.length > 0 ? result.data[0] : null;
+                if (latestTransaction) {
+            res.json(latestTransaction);
+        } else {
+            res.json({ message: "No transactions found." });
         }
-    }), { 'userJid': ownerBot });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
-    await vreden.relayMessage(ownerBot, msg.message, {
-        'participant': {
-            'jid': ownerBot
-        },
-        'messageId': msg.key.id
-    });
-}
-global.owner = "6285216955233@s.whatsapp.net"
