af1910f0b0dd9d3959faa3a52398be9fe4636f59
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2024-12-14 21:56:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -104,6 +104,60 @@ app.get('/api/download/ytdl', async (req, res) => {
   }
 });
 
+// API ORKUT CREATE DEPOSIT
+app.get('/api/orkut/createpayment', async (req, res) => {
+    const { apikey } = req.query;    
+    if (apikey !== 'linebaik') {
+    return res.status(403).json({ error: "Isi Parameter Apikey" });
+    }
+    const { amount } = req.query;
+    if (!amount) {
+    return res.json("Isi Parameter Amount.");
+    }
+    const { codeqr } = req.query;
+    if (!codeqr) {
+    return res.json("Isi Parameter CodeQr menggunakan qris code kalian.");
+    }
+    try {
+        const qrData = await createQRIS(amount, codeqr);
+        res.json({ status: true, creator: "Line", result: qrData });        
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// CEK STATUS 
+app.get('/api/orkut/cekstatus', async (req, res) => {
+    const { apikey, merchant, keyorkut } = req.query;
+    
+    if (apikey !== 'linebaik') {
+    return res.status(403).json({ error: "Isi Parameter Apikey" });
+    }
+    
+        if (!merchant) {
+        return res.json({ error: "Isi Parameter Merchant." });
+    }
+    if (!keyorkut) {
+        return res.json({ error: "Isi Parameter Token menggunakan token kalian." });
+    }
+    try {
+        const apiUrl = `https://gateway.okeconnect.com/api/mutasi/qris/${merchant}/${keyorkut}`;
+        const response = await axios.get(apiUrl);
+        const result = response.data;
+                // Check if data exists and get the latest transaction
+        const latestTransaction = result.data && result.data.length > 0 ? result.data[0] : null;
+                if (latestTransaction) {
+            res.json(latestTransaction);
+        } else {
+            res.json({ message: "No transactions found." });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
+
 // Penanganan error untuk route yang tidak ditemukan
 app.use((req, res, next) => {
   res.status(404).send("Halaman tidak ditemukan.");
@@ -119,5 +173,6 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server berjalan pada port ${PORT}`);
 });
+
 
 module.exports = app;
