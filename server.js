e0955c3ebe5e60ed041e1a4d5e52890a0aac99ae
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2024-12-14 21:32:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const {
+  convertCRC16,
+  generateTransactionId,
+  generateExpirationTime,
+  elxyzFile,
+  generateQRIS,
+  createQRIS,
+  checkQRISStatus
+} = require('./lib/orkut.js')
 
 app.use(express.static(path.join(__dirname, 'public')));
 
