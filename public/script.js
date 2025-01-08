4356b1e2a717e342a2c1f959fed89c1ff66d51a8
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2025-01-08 20:41:41 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,254 +1,257 @@
-function toggleNav() {
-    var sidebar = document.getElementById("mySidebar");
-    var menuIcon = document.getElementById("menuIcon");
-    var overlay = document.getElementById("overlay");
-    if (sidebar.style.width === "500px") {
-        sidebar.style.width = "0";
-        overlay.style.display = "none";
-        menuIcon.classList.remove("fa-times");
-        menuIcon.classList.add("fa-bars");
-        menuIcon.style.transform = "rotate(360deg)";
-        setTimeout(function() {
-            menuIcon.style.transform = "rotate(0deg)";
-        }, 500);
-    } else {
-        sidebar.style.width = "500px";
-        overlay.style.display = "block";
-        menuIcon.classList.remove("fa-bars");
-        menuIcon.classList.add("fa-times");
-        menuIcon.style.transform = "rotate(360deg)";
-        setTimeout(function() {
-            menuIcon.style.transform = "rotate(0deg)";
-        }, 500);
-    }
-}
+const menuToggle = document.getElementById('menuToggle');
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById('overlay');
 
-function toggleProfilePopup() {
-    var profilePopup = document.getElementById("profilePopup");
-    var profileIcon = document.getElementById("profileIcon");
-    var overlay = document.getElementById("overlay");
-    if (profilePopup.style.display === "block") {
-        profilePopup.classList.remove("show");
-        setTimeout(function() {
-            profilePopup.style.display = "none";
-            overlay.style.display = "none";
-        }, 500);
-        profileIcon.style.transform = "rotate(360deg)";
-        setTimeout(function() {
-            profileIcon.style.transform = "rotate(0deg)";
-        }, 500);
-    } else {
-        profilePopup.style.display = "block";
-        overlay.style.display = "block";
-        setTimeout(function() {
-            profilePopup.classList.add("show");
-        }, 10);
-        profileIcon.style.transform = "rotate(360deg)";
-        setTimeout(function() {
-            profileIcon.style.transform = "rotate(0deg)";
-        }, 500);
-    }
-}
+menuToggle.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+  overlay.classList.toggle('active');
 
-function closeAll() {
-    var sidebar = document.getElementById("mySidebar");
-    var menuIcon = document.getElementById("menuIcon");
-    var overlay = document.getElementById("overlay");
-    var profilePopup = document.getElementById("profilePopup");
-    var profileIcon = document.getElementById("profileIcon");
-    sidebar.style.width = "0";
-    profilePopup.classList.remove("show");
-    setTimeout(function() {
-        profilePopup.style.display = "none";
-        overlay.style.display = "none";
-    }, 500);
-    menuIcon.classList.remove("fa-times");
-    menuIcon.classList.add("fa-bars");
-    menuIcon.style.transform = "rotate(360deg)";
-    setTimeout(function() {
-        menuIcon.style.transform = "rotate(0deg)";
-    }, 500);
-    profileIcon.style.transform = "rotate(360deg)";
-    setTimeout(function() {
-        profileIcon.style.transform = "rotate(0deg)";
-    }, 500);
-}
-
-function closeNotification() {
-    var notification = document.getElementById("notification");
-    notification.style.display = "none";
-}
-
-window.addEventListener("load", function() {
-    var container = document.querySelector(".container");
-    container.style.opacity = "1";
+  // Change icon and rotation
+  if (menuToggle.classList.contains('active')) {
+    menuToggle.classList.remove('active');
+    menuToggle.classList.replace('fa-times', 'fa-bars');
+  } else {
+    menuToggle.classList.add('active');
+    menuToggle.classList.replace('fa-bars', 'fa-times');
+  }
 });
 
-function openLink(button) {
-    var url = button.getAttribute("data-url");
-    window.open(url, "_blank");
+overlay.addEventListener('click', () => {
+  sidebar.classList.remove('active');
+  overlay.classList.remove('active');
+  
+  menuToggle.classList.remove('active');
+  menuToggle.classList.replace('fa-times', 'fa-bars');
+});
+
+function getCurrentTime() {
+  return new Date().getTime();
 }
 
+function getVisitorCount() {
+  const count = localStorage.getItem('visitorCount');
+  return count ? parseInt(count, 10) : 0;
+}
 
-//=====[ FUNCTION JUMLAH PENGUNJUNG ]======//
-        function getCurrentTime() {
-            return new Date().getTime();
-        }
+function setVisitorCount(count) {
+  localStorage.setItem('visitorCount', count);
+}
 
-        function getVisitorCount() {
-            const count = localStorage.getItem('visitorCount');
-            return count ? parseInt(count, 10) : 0;
-        }
+function setResetTime() {
+  localStorage.setItem('resetTime', getCurrentTime());
+}
 
-        function setVisitorCount(count) {
-            localStorage.setItem('visitorCount', count);
-        }
+function getResetTime() {
+  return localStorage.getItem('resetTime');
+}
 
-        function setResetTime() {
-            localStorage.setItem('resetTime', getCurrentTime());
-        }
+function isResetTimePassed() {
+  const lastReset = getResetTime();
+  const currentTime = getCurrentTime();
+  return !lastReset || (currentTime - lastReset > 3600000); // 60 menit = 3600000 ms
+}
 
-        function getResetTime() {
-            return localStorage.getItem('resetTime');
-        }
+function handleVisitorCount() {
+  if (isResetTimePassed()) {
+    setVisitorCount(0);
+    setResetTime();
+  } else {
+    const currentCount = getVisitorCount();
+    setVisitorCount(currentCount + 1);
+  }
+  updateVisitorCountDisplay();
+}
 
-        function isResetTimePassed() {
-            const lastReset = getResetTime();
-            const currentTime = getCurrentTime();
-            return !lastReset || (currentTime - lastReset > 3600000); // 60 menit = 3600000 ms
-        }
+function updateVisitorCountDisplay() {
+  const count = getVisitorCount();
+  document.getElementById('visitorCount').innerText = count;
+}
 
-        function handleVisitorCount() {
-            if (isResetTimePassed()) {
-                setVisitorCount(0); 
-                setResetTime();      
-            } else {
-                const currentCount = getVisitorCount();
-                setVisitorCount(currentCount + 1);  
-            }
-            updateVisitorCountDisplay();
-        }
+function getFormattedDate() {
+  const now = new Date();
+  const daysOfWeek = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+  const dayOfWeek = daysOfWeek[now.getDay()];
+  const day = now.getDate();
+  const month = now.getMonth() + 1;
+  const year = now.getFullYear();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const milliseconds = now.getMilliseconds();
+  const formattedDate = `${dayOfWeek}, ${day}/${month}/${year} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+  return formattedDate;
+}
 
-        function updateVisitorCountDisplay() {
-            const count = getVisitorCount();
-            document.getElementById('visitorCount').innerText = count;
-        }
+function updateClock() {
+  document.getElementById('dateTime').innerText = getFormattedDate();
+}
 
-        function getFormattedDate() {
-            const now = new Date();
-            const daysOfWeek = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
-            const dayOfWeek = daysOfWeek[now.getDay()];
-            const day = now.getDate();
-            const month = now.getMonth() + 1;
-            const year = now.getFullYear();
-            const hours = now.getHours();
-            const minutes = now.getMinutes();
-            const seconds = now.getSeconds();
-            const milliseconds = now.getMilliseconds();
-            const formattedDate = `${dayOfWeek}, ${day}/${month}/${year} ${hours}:${minutes}:${seconds}.${milliseconds}`;
-            return formattedDate;
-        }
-        function updateClock() {
-            document.getElementById('dateTime').innerText = getFormattedDate();
-        }
-        setInterval(updateClock, 1);
-        handleVisitorCount();
-        
-        //==== GET IP
-        fetch('https://api.ipify.org?format=json')
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('visitorIP').innerText = data.ip;
-            })
-            .catch(error => {
-                document.getElementById('visitorIP').innerText = 'Tidak dapat mendeteksi IP';
-                console.error('Error:', error);
-            });
+setInterval(updateClock, 1);
+handleVisitorCount();
 
-        //==== KECEPATAN JARINGAN
-        var startTime;
-        var downloadSize = 500000;
-        var downloadUrl = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_92x30dp.png";
-        var image = new Image();
-        var speedDisplayElement = document.getElementById('networkSpeed');
-        var currentSpeed = 0;
-        function measureNetworkSpeed() {
-            startTime = (new Date()).getTime();
-            image.src = downloadUrl + "?cacheBuster=" + startTime; 
-        }
-        function calculateSpeed(duration) {
-            var bitsLoaded = downloadSize * 8; 
-            var speedBps = (bitsLoaded / duration) * 1000;
-            var speedKbps = speedBps / 1024; 
-            var speedMbps = speedKbps / 1024; 
-            if (speedKbps < 1024) {
-                currentSpeed = speedKbps.toFixed(2) + " KB/0.5s";
-            } else {
-                currentSpeed = speedMbps.toFixed(2) + " MB/0.5s";
-            }
-            speedDisplayElement.innerText = currentSpeed;
-        }
-        function updateNetworkSpeed() {
-            measureNetworkSpeed();
-            image.onload = function() {
-                var endTime = (new Date()).getTime(); 
-                var duration = endTime - startTime;
-                calculateSpeed(duration);
-            };
-            image.onerror = function() {
-                speedDisplayElement.innerText = "Gagal mengukur kecepatan.";
-            };
-        }
-        setInterval(updateNetworkSpeed, 500);
+//==== GET IP
+fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('visitorIP').innerText = data.ip;
+  })
+  .catch(error => {
+    document.getElementById('visitorIP').innerText = 'Tidak dapat mendeteksi IP';
+    console.error('Error:', error);
+  });
 
-        //==== GET STATUS BATRAI 
-        function updateBatteryStatus(battery) {
-            const batteryPercentage = battery.level * 100;
-            const chargingStatus = battery.charging ? "Sedang mengisi daya" : "Tidak sedang mengisi daya";
-            document.getElementById('batteryStatus').innerHTML = `${batteryPercentage}%<br>Status:${chargingStatus}`;
-        }
-        if ('getBattery' in navigator) {
-            navigator.getBattery().then(function(battery) {
-                battery.addEventListener('levelchange', function() {
-                    updateBatteryStatus(battery);
-                });
-                battery.addEventListener('chargingchange', function() {
-                    updateBatteryStatus(battery);
-                });
-                setInterval(function() {
-                    updateBatteryStatus(battery);
-                }, 10);
-            });
-        } else {
-            document.getElementById('batteryStatus').innerHTML = "Battery Status API tidak didukung di browser ini.";
-        }
+//==== KECEPATAN JARINGAN
+var startTime;
+var downloadSize = 500000;
+var downloadUrl = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_92x30dp.png";
+var image = new Image();
+var speedDisplayElement = document.getElementById('networkSpeed');
+var currentSpeed = 0;
 
+function measureNetworkSpeed() {
+  startTime = (new Date()).getTime();
+  image.src = downloadUrl + "?cacheBuster=" + startTime;
+}
 
+function calculateSpeed(duration) {
+  var bitsLoaded = downloadSize * 8;
+  var speedBps = (bitsLoaded / duration) * 1000;
+  var speedKbps = speedBps / 1024;
+  var speedMbps = speedKbps / 1024;
+  
+  if (speedKbps < 1024) {
+    currentSpeed = speedKbps.toFixed(2) + " KB/0.5s";
+  } else {
+    currentSpeed = speedMbps.toFixed(2) + " MB/0.5s";
+  }
+  speedDisplayElement.innerText = currentSpeed;
+}
 
+function updateNetworkSpeed() {
+  measureNetworkSpeed();
+  image.onload = function() {
+    var endTime = (new Date()).getTime();
+    var duration = endTime - startTime;
+    calculateSpeed(duration);
+  };
+  image.onerror = function() {
+    speedDisplayElement.innerText = "Gagal mengukur kecepatan.";
+  };
+}
 
+setInterval(updateNetworkSpeed, 500);
 
-        //===== js fitur 
+//==== GET STATUS BATRAI
+function updateBatteryStatus(battery) {
+  const batteryPercentage = battery.level * 100;
+  const chargingStatus = battery.charging ? "Sedang mengisi daya" : "Tidak sedang mengisi daya";
+  document.getElementById('batteryStatus').innerHTML = `${batteryPercentage}%<br>Status:${chargingStatus}`;
+}
 
-        function toggleDropdown(button) {
-            const row = button.closest("tr");
-            const dropdownContent = row.nextElementSibling;
-      
-            if (dropdownContent.style.display === "table-row") {
-              dropdownContent.style.display = "none";
-              button.querySelector("i").className = "fas fa-chevron-down";
-            } else {
-              dropdownContent.style.display = "table-row";
-              button.querySelector("i").className = "fas fa-chevron-up";
-            }
-          }
-      
-          function copyText() {
-              var text = "text yang akan di salin"; 
-              var tempInput = document.createElement("input");
-              document.body.appendChild(tempInput);
-              tempInput.value = text;
-              tempInput.select();
-              document.execCommand('copy');
-              document.body.removeChild(tempInput);
-              alert("Teks telah disalin ke clipboard!");
-            }
+if ('getBattery' in navigator) {
+  navigator.getBattery().then(function(battery) {
+    battery.addEventListener('levelchange', function() {
+      updateBatteryStatus(battery);
+    });
+    battery.addEventListener('chargingchange', function() {
+      updateBatteryStatus(battery);
+    });
+    setInterval(function() {
+      updateBatteryStatus(battery);
+    }, 10);
+  });
+} else {
+  document.getElementById('batteryStatus').innerHTML = "Battery Status API tidak didukung di browser ini.";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll('.animated');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  });
+
+  elements.forEach((el) => observer.observe(el));
+});
+
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 350,
+      density: {
+        enable: true,
+        value_area: 1500,
+      },
+    },
+    color: {
+      value: "#ADD8E6",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+    },
+    opacity: {
+      value: 0.6,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 10,
+      random: true,
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.6,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "grab",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 150,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      push: {
+        particles_nb: 5,
+      },
+    },
+  },
+  retina_detect: true,
+});
